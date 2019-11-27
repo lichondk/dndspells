7e692b84a56eb4f375254ad0bf85a64ba41524a2
lichondk <lichondk@gmail.com>
2019-11-27 17:20:05 +0100

    tset

--- Server.js
+++ Server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const rout = require('./rout.js')
 const Spells = require('./spellsModel');
 const app = express();
 const port = process.env.PORT || process.argv[2] || 8080;
@@ -12,6 +11,5 @@ app.use('/spells', (req, res) => {
 
 app.get('/', (req, res) => res.send('hello world!'))
 
-app.get('/test', (req, res) => res.send('test'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
