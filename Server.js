81d12c1037603636394fb11b8c691f21002cfa56
lichondk <lichondk@gmail.com>
2019-11-28 15:50:12 +0100

    cors

--- Server.js
+++ Server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const Spells = require('./spellsModel');
 const app = express();
+
 const port = process.env.PORT || process.argv[2] || 8080;
 
-app.use('/spells', (req, res) => {
+app.use(cors({
+  origin: 'https://dnddevfrontend.herokuapp.com/'
+}));
+
+app.get('/spells', (req, res) => {
     Spells.find().then(function(data){
         res.send(data)
       });
 });
+
+
 
 app.get('/', (req, res) => res.send('hello world!'))
 
